61be1ba7f541f1e8c1158a0e9df7c8ba92424b6b
Jorge Sarricolea <jjorgesarricolea18@gmail.com>
2023-04-24 12:46:10 -0600

    Card effects fixed

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
-const card = document.querySelector('.card');
+let images = document.querySelectorAll('.card img');
+let cards = document.querySelectorAll('.card');
 
-card.addEventListener('click',function () {
-    this.classList.toggle('active');
+images.forEach(function (image, index) {
+    image.addEventListener('click', function () {
+        cards[index].classList.toggle('active');
+        let row = event.target.closest('.row');
+        if (row) {
+            row.classList.toggle('rowdirection');
+        }
+        let activeImage = document.querySelector('.card.active img');
+        if (activeImage) {
+            activeImage.scrollIntoView();
+        } else {
+            let header = document.querySelector('header');
+            if (header) {
+                header.scrollIntoView();
+            }
+        }
+    });
 });
